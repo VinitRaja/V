0799d3ee17551512689f2a30fef7e2d6795857ab
Vinit Raja <60826741+VinitRaja@users.noreply.github.com>
2020-02-09 01:42:15 +0530

    Adding of two numbers in android

--- MainActivity.java
+++ MainActivity.java
@@ -1,53 +1,13 @@
-package mynewproject.vinit.com.logindemo;
+package com.example.android.addingtwonumber;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-    private EditText Name;
-    private EditText Password;
-    private TextView Info;
-    private Button Login;
-    private int counter = 5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Name = (EditText)findViewById(R.id.etName);
-        Password = (EditText)findViewById(R.id.etPassword);
-        Info = (TextView)findViewById(R.id.tvInfo);
-        Login = (Button)findViewById(R.id.btnlogin);
-
-        Info.setText("No. of Attempt remaining: 5");
-
-        Login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                validate(Name.getText().toString(), Password.getText().toString());
-
-            }
-        });
-
-    }
-    private void validate (String userName, String userPassword){
-        if((userName == "Admin") && (userPassword == "1234")){
-            Intent intent = new Intent(MainActivity.this,SecondActivity.class);
-            startActivity(intent);
-        }else{
-            counter--;
-            Info.setText("No. of Attempts Remaining: "+ String.valueOf(counter));
-
-            if(counter == 0){
-                Login.setEnabled(false);
-            }
-        }
     }
 }
